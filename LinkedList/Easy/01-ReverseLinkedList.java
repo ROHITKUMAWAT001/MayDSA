07ad1aad8668808e6ae7f24ffcbdec7b6560c10e
ROHIT kumawat 101 <204474860+ROHITKUMAWAT001@users.noreply.github.com>
2026-05-06 16:35:28 +0530

    recursive reverse

--- LinkedList/Easy/01-ReverseLinkedList.java
+++ LinkedList/Easy/01-ReverseLinkedList.java
@@ -1,3 +1,5 @@
+
+
 /*
 Problem: Reverse Linked List
 Platform: LeetCode
@@ -38,5 +40,20 @@ class Solution {
         }
 
         return prev;
+    }
+
+    public static ListNode reverse(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+         
+     ListNode newHead =    reverse(head.next);
+        head.next.next = head;
+       head.next= null;
+
+       return newHead;
+    
+        
+
     }
 }
